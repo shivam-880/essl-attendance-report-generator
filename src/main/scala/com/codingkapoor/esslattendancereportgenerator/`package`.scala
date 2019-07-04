4feba3b98fe47aebfef6f9888779231261561c54
codingkapoor <mailtoshivamk@gmail.com>
2019-07-05 02:14:06 +0530

    ref

--- src/main/scala/com/codingkapoor/esslattendancereportgenerator/`package`.scala
+++ src/main/scala/com/codingkapoor/esslattendancereportgenerator/`package`.scala
@@ -4,6 +4,10 @@ object `package` {
   val CompanyName = "Glassbeam Software India Pvt Ltd"
   val CompanyAddress = "No. 21, 3rd Floor, Block A, Sree Rama Deevana, Halasuru Road, Bangalore-42"
 
+  val EmployeesInfoHeader = Array("Employee Name", "Gender", "Date Of Joining", "PF No.")
+
+  val AttendanceReportFileName = "gbatt.xlsx"
+
   def using[A <: {def close() : Unit}, B](resource: A)(f: A => B): B = {
     try {
       f(resource)
--- src/main/scala/com/codingkapoor/esslattendancereportgenerator/core/ExcelWriter.scala
+++ src/main/scala/com/codingkapoor/esslattendancereportgenerator/core/ExcelWriter.scala
@@ -11,60 +11,59 @@ import com.codingkapoor.esslattendancereportgenerator._
 object ExcelWriter {
 
   def write(employees: Seq[Employee]): Unit = {
-    val columns = Array("Employee Name", "Gender", "Date Of Joining", "PF No.")
 
-    val workbook = new XSSFWorkbook
-    val createHelper = workbook.getCreationHelper
+    using(new XSSFWorkbook) { workbook =>
 
-    val sheet = workbook.createSheet("July")
+      val createHelper = workbook.getCreationHelper
 
-    val headerFont = workbook.createFont
-    headerFont.setBold(true)
-    headerFont.setFontHeightInPoints(11.toShort)
-    headerFont.setColor(IndexedColors.BLACK.getIndex)
+      val sheet = workbook.createSheet("July")
 
-    val headerCellStyle = workbook.createCellStyle
-    headerCellStyle.setFont(headerFont)
+      val headerFont = workbook.createFont
+      headerFont.setBold(true)
+      headerFont.setFontHeightInPoints(11.toShort)
+      headerFont.setColor(IndexedColors.BLACK.getIndex)
 
-    val headerRow1 = sheet.createRow(0)
-    headerRow1.setHeightInPoints(50)
-    val cell1 = headerRow1.createCell(0)
-    cell1.setCellValue(s"$CompanyName\n$CompanyAddress")
-    cell1.setCellStyle(headerCellStyle)
+      val headerCellStyle = workbook.createCellStyle
+      headerCellStyle.setFont(headerFont)
 
-    sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 4))
+      val headerRow1 = sheet.createRow(0)
+      headerRow1.setHeightInPoints(50)
+      val cell1 = headerRow1.createCell(0)
+      cell1.setCellValue(s"$CompanyName\n$CompanyAddress")
+      cell1.setCellStyle(headerCellStyle)
 
-    val headerRow = sheet.createRow(2)
+      sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 4))
 
-    for (i <- columns.indices) {
-      val cell = headerRow.createCell(i)
-      cell.setCellValue(columns(i))
-      cell.setCellStyle(headerCellStyle)
+      val headerRow = sheet.createRow(2)
+
+      for (i <- EmployeesInfoHeader.indices) {
+        val cell = headerRow.createCell(i)
+        cell.setCellValue(EmployeesInfoHeader(i))
+        cell.setCellStyle(headerCellStyle)
+      }
+
+      val dateCellStyle = workbook.createCellStyle
+      dateCellStyle.setDataFormat(createHelper.createDataFormat.getFormat("dd-MM-yyyy"))
+
+      var rowNum = 3
+      for (employee <- employees) {
+        val row = sheet.createRow(rowNum)
+        row.createCell(0).setCellValue(employee.name)
+        row.createCell(1).setCellValue(employee.gender)
+        val doj = row.createCell(2)
+        doj.setCellValue(employee.doj.toString)
+        doj.setCellStyle(dateCellStyle)
+        row.createCell(3).setCellValue(employee.pfn)
+
+        rowNum += 1
+      }
+
+      for (i <- EmployeesInfoHeader.indices)
+        sheet.autoSizeColumn(i)
+
+      using(new FileOutputStream(AttendanceReportFileName)) { os =>
+        workbook.write(os)
+      }
     }
-
-    val dateCellStyle = workbook.createCellStyle
-    dateCellStyle.setDataFormat(createHelper.createDataFormat.getFormat("dd-MM-yyyy"))
-
-    var rowNum = 3
-    for (employee <- employees) {
-      val row = sheet.createRow(rowNum)
-      row.createCell(0).setCellValue(employee.name)
-      row.createCell(1).setCellValue(employee.gender)
-      val doj = row.createCell(2)
-      doj.setCellValue(employee.doj.toString)
-      doj.setCellStyle(dateCellStyle)
-      row.createCell(3).setCellValue(employee.pfn)
-
-      rowNum += 1
-    }
-
-    for (i <- columns.indices)
-      sheet.autoSizeColumn(i)
-
-    val fileOut: FileOutputStream = new FileOutputStream("gbatt.xlsx")
-    workbook.write(fileOut)
-    fileOut.close()
-
-    workbook.close()
   }
 }
