1bc6915d500f92e61cbefe9e520223044385f4a5
codingkapoor <mailtoshivamk@gmail.com>
2019-07-05 12:03:00 +0530

    report first version

--- src/main/scala/com/codingkapoor/esslattendancereportgenerator/core/ExcelWriter.scala
+++ src/main/scala/com/codingkapoor/esslattendancereportgenerator/core/ExcelWriter.scala
@@ -13,33 +13,30 @@ object ExcelWriter {
   def write(employees: Seq[Employee]): Unit = {
 
     using(new XSSFWorkbook) { workbook =>
-
-      val createHelper = workbook.getCreationHelper
-
       val sheet = workbook.createSheet("July")
+      val createHelper = workbook.getCreationHelper
 
       val headerFont = workbook.createFont
       headerFont.setBold(true)
-      headerFont.setFontHeightInPoints(11.toShort)
+      headerFont.setFontHeightInPoints(10.5.toShort)
       headerFont.setColor(IndexedColors.BLACK.getIndex)
 
       val headerCellStyle = workbook.createCellStyle
       headerCellStyle.setFont(headerFont)
 
-      val headerRow1 = sheet.createRow(0)
-      headerRow1.setHeightInPoints(50)
-      val cell1 = headerRow1.createCell(0)
-      cell1.setCellValue(s"$CompanyName\n$CompanyAddress")
-      cell1.setCellStyle(headerCellStyle)
+      val companyDetailsRow = sheet.createRow(0)
+      companyDetailsRow.setHeightInPoints(50)
+      val companyDetailsCol = companyDetailsRow.createCell(0)
+      companyDetailsCol.setCellValue(s"$CompanyName\n$CompanyAddress")
+      companyDetailsCol.setCellStyle(headerCellStyle)
 
-      sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 4))
+      sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 3))
 
-      val headerRow = sheet.createRow(2)
-
+      val employeesInfoHeaderRow = sheet.createRow(2)
       for (i <- EmployeesInfoHeader.indices) {
-        val cell = headerRow.createCell(i)
-        cell.setCellValue(EmployeesInfoHeader(i))
-        cell.setCellStyle(headerCellStyle)
+        val employeesInfoHeaderCell = employeesInfoHeaderRow.createCell(i)
+        employeesInfoHeaderCell.setCellValue(EmployeesInfoHeader(i))
+        employeesInfoHeaderCell.setCellStyle(headerCellStyle)
       }
 
       val dateCellStyle = workbook.createCellStyle
@@ -47,13 +44,13 @@ object ExcelWriter {
 
       var rowNum = 3
       for (employee <- employees) {
-        val row = sheet.createRow(rowNum)
-        row.createCell(0).setCellValue(employee.name)
-        row.createCell(1).setCellValue(employee.gender)
-        val doj = row.createCell(2)
+        val employeeInfoRow = sheet.createRow(rowNum)
+        employeeInfoRow.createCell(0).setCellValue(employee.name)
+        employeeInfoRow.createCell(1).setCellValue(employee.gender)
+        val doj = employeeInfoRow.createCell(2)
         doj.setCellValue(employee.doj.toString)
         doj.setCellStyle(dateCellStyle)
-        row.createCell(3).setCellValue(employee.pfn)
+        employeeInfoRow.createCell(3).setCellValue(employee.pfn)
 
         rowNum += 1
       }
