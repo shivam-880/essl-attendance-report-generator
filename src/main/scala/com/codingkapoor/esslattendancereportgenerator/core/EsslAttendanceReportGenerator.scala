93d6ed25c80fa1a1086b3a5e3574661e6c4e2085
codingkapoor <mailtoshivamk@gmail.com>
2019-07-05 16:43:08 +0530

    renamed attendance to attlogs

--- src/main/scala/com/codingkapoor/esslattendancereportgenerator/core/EsslAttendanceReportGenerator.scala
+++ src/main/scala/com/codingkapoor/esslattendancereportgenerator/core/EsslAttendanceReportGenerator.scala
@@ -2,30 +2,30 @@ package com.codingkapoor.esslattendancereportgenerator.core
 
 import java.time.LocalDate
 
-import com.codingkapoor.esslattendancereportgenerator.model.{Attendance, Employee, Holiday, Request}
+import com.codingkapoor.esslattendancereportgenerator.model.{AttLog, Employee, Holiday, Request}
 import com.typesafe.scalalogging.LazyLogging
 
 // App you reply with an error message if uptodate attlog or holidays is not provided
 // Also notify entries in attlog that were not mentioned in employees.json
 object EsslAttendanceReportGenerator extends App with LazyLogging with ConfigLoader {
   val (month, year) = getConfiguredMonthYear
-  logger.debug(s"Configured month = $month, year = $year")
+  logger.debug(s"month = $month, year = $year")
 
   val holidays = Holiday.getHolidays.filter(h => h.date.getMonthValue == month && h.date.getYear == year)
-  logger.debug(s"Holidays = $holidays")
+  logger.debug(s"holidays = $holidays")
 
   val employees = Employee.getEmployees
-  logger.debug(s"Employees = $employees")
+  logger.debug(s"employees = $employees")
 
-  val attendance = Attendance.getAttendance.foldLeft(Map[Int, List[LocalDate]]()) { (acc, att) =>
+  val attLogs = AttLog.getAttLogs.foldLeft(Map[Int, List[LocalDate]]()) { (acc, att) =>
     if (acc.contains(att.empId)) {
       acc + (att.empId -> (att.date :: acc(att.empId)).sortWith(_.isBefore(_)))
     } else acc + (att.empId -> List(att.date).sortWith(_.isBefore(_)))
   }
-  logger.debug(s"Attendance = $attendance")
+  logger.debug(s"attLogs = $attLogs")
 
   val requests = Request.getRequests
-  logger.debug(s"Requests = $requests")
+  logger.debug(s"requests = $requests")
 
   ExcelWriter.write(month, year, employees)
 }
--- src/main/scala/com/codingkapoor/esslattendancereportgenerator/model/AttLog.scala
+++ src/main/scala/com/codingkapoor/esslattendancereportgenerator/model/AttLog.scala
@@ -7,21 +7,21 @@ import com.codingkapoor.esslattendancereportgenerator.core.RuntimeEnvironment
 
 import scala.io.Source
 
-case class Attendance(empId: Int, date: LocalDate) extends Ordered[Attendance] {
-  override def compare(that: Attendance): Int = {
+case class AttLog(empId: Int, date: LocalDate) extends Ordered[AttLog] {
+  override def compare(that: AttLog): Int = {
     if (this.date == that.date) 0
     else if (this.date.isBefore(that.date)) -1
     else 1
   }
 }
 
-object Attendance {
-  def getAttendance: Set[Attendance] = {
+object AttLog {
+  def getAttLogs: Set[AttLog] = {
     using(Source.fromFile(s"${RuntimeEnvironment.getDataDir}/1_attlog.dat")) { attlog =>
       attlog.getLines().toList.filter(l => l.trim.length > 0).map { line =>
         line.trim.split("\\s+") match {
           case Array(empId, date, _, _, _, _, _) =>
-            Attendance(empId.toInt, LocalDate.parse(date))
+            AttLog(empId.toInt, LocalDate.parse(date))
         }
       }
     }.toSet
