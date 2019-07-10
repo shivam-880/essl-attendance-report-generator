0e46f49b90b3ec16bc3647fd47302aa51cee0a68
codingkapoor <mailtoshivamk@gmail.com>
2019-07-10 18:48:15 +0530

    holiday dimensions impl

--- src/main/scala/com/codingkapoor/esslattendancereportgenerator/writer/holiday/HolidayWriter.scala
+++ src/main/scala/com/codingkapoor/esslattendancereportgenerator/writer/holiday/HolidayWriter.scala
@@ -3,6 +3,7 @@ package com.codingkapoor.esslattendancereportgenerator.writer.holiday
 import java.time.YearMonth
 
 import com.codingkapoor.esslattendancereportgenerator.model.{AttendancePerEmployee, Holiday}
+import com.codingkapoor.esslattendancereportgenerator.writer.AttendanceDimensions
 import org.apache.poi.ss.util.CellRangeAddress
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
@@ -18,31 +19,34 @@ trait HolidayWriter extends HolidayStyle {
 
     val sheet = workbook.getSheet(_month)
 
+    val attendanceDimensions = AttendanceDimensions(month, year, attendances.map(l => l.employee))
+
+    val mergedRegionfirstRowIndex = attendanceDimensions.firstRowIndex
+    val mergedRegionlastRowIndex = attendanceDimensions.lastRowIndex - 1
+
     val cellStyle = getHolidayCellStyle
 
     for (holiday <- holidays) {
       val day = holiday.date.getDayOfMonth
-      val dayIndex = 5 + day - 1
+      val dayIndex = attendanceDimensions.firstColumnIndex + day - 1
 
-      val firstRowIndex = 3
-      val lastRowIndex = firstRowIndex + attendances.size - 1
-      val firstColIndex = dayIndex
-      val lastColIndex = dayIndex
+      val mergedRegionfirstColumnIndex = dayIndex
+      val mergedRegionlastColumnIndex = dayIndex
 
-      if (!mergedRegionAlreadyExists(firstRowIndex, lastRowIndex, firstColIndex, lastColIndex)) {
+      if (!mergedRegionAlreadyExists(mergedRegionfirstRowIndex, mergedRegionlastRowIndex, mergedRegionfirstColumnIndex, mergedRegionlastColumnIndex)) {
 
-        for (i <- firstRowIndex to lastRowIndex) {
+        for (i <- mergedRegionfirstRowIndex to mergedRegionlastRowIndex) {
           val row = sheet.getRow(i)
-          for (j <- firstColIndex to lastColIndex) {
+          for (j <- mergedRegionfirstColumnIndex to mergedRegionlastColumnIndex) {
             val col = row.createCell(j)
             col.setCellStyle(cellStyle)
-            if (i == firstRowIndex && j == firstColIndex) {
+            if (i == mergedRegionfirstRowIndex && j == mergedRegionfirstColumnIndex) {
               col.setCellValue(holiday.occasion)
             }
           }
         }
 
-        sheet.addMergedRegion(new CellRangeAddress(firstRowIndex, lastRowIndex, firstColIndex, lastColIndex))
+        sheet.addMergedRegion(new CellRangeAddress(mergedRegionfirstRowIndex, mergedRegionlastRowIndex, mergedRegionfirstColumnIndex, mergedRegionlastColumnIndex))
       }
     }
   }
