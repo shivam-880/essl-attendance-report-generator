e32f885bab590ca7adc353604a3cae0ae9300bb5
codingkapoor <mailtoshivamk@gmail.com>
2019-07-10 19:53:31 +0530

    ref

--- src/main/scala/com/codingkapoor/esslattendancereportgenerator/core/EsslAttendanceReportGenerator.scala
+++ src/main/scala/com/codingkapoor/esslattendancereportgenerator/core/EsslAttendanceReportGenerator.scala
@@ -1,12 +1,7 @@
 package com.codingkapoor.esslattendancereportgenerator.core
 
-import java.time.LocalDate
-
-import com.codingkapoor.esslattendancereportgenerator.model._
 import com.codingkapoor.esslattendancereportgenerator.writer._
 import com.typesafe.scalalogging.LazyLogging
-
-import scala.collection.immutable
 
 // App you reply with an error message if uptodate attlog or holidays is not provided
 // Also notify entries in attlog that were not mentioned in employees.json
@@ -15,37 +10,5 @@ object EsslAttendanceReportGenerator extends App with LazyLogging with ConfigLoa
   val (month, year) = getConfiguredMonthYear
   logger.debug(s"month = $month, year = $year")
 
-  val holidays = Holiday.getHolidays.filter(h => h.date.getMonthValue == month && h.date.getYear == year)
-  logger.debug(s"holidays = $holidays")
-
-  val employees = Employee.getEmployees
-  logger.debug(s"employees = $employees")
-
-  val attendanceLogs: Map[Int, List[LocalDate]] = AttendanceLog.getAttendanceLogs.foldLeft(Map[Int, List[LocalDate]]()) { (acc, att) =>
-    if (acc.contains(att.empId)) {
-      acc + (att.empId -> (att.date :: acc(att.empId)).sortWith(_.isBefore(_)))
-    } else acc + (att.empId -> List(att.date).sortWith(_.isBefore(_)))
-  }
-  logger.debug(s"attendanceLogs = $attendanceLogs")
-
-  val requests = Request.getRequests
-  logger.debug(s"requests = $requests")
-
-  val attendances: immutable.Seq[AttendancePerEmployee] = employees.map { employee =>
-    val attendance = attendanceLogs(employee.empId) // handle key not found
-    val _requests: List[Request] = requests.filter(l => l.empId == employee.empId)
-    val r = AttendancePerEmployee.getAttendancePerEmployee(employee, attendance, holidays, _requests)(month, year)
-    logger.debug(s"emp = ${employee.empId}, r = ${r.attendance.toList.sortWith(_._1 < _._1)}")
-
-    r
-  }
-
-  println(CompanyDetailsDimensions())
-  println(SectionHeaderDimensions(month, year))
-  println(EmployeeInfoHeaderDimensions())
-  println(EmployeeInfoDimensions(employees))
-  println(AttendanceHeaderDimensions(month, year))
-  println(AttendanceDimensions(month, year, employees))
-
-  ExcelWriter(month, year, attendances, holidays).write()
+  ExcelWriter(month, year).write()
 }
--- src/main/scala/com/codingkapoor/esslattendancereportgenerator/writer/ExcelWriter.scala
+++ src/main/scala/com/codingkapoor/esslattendancereportgenerator/writer/ExcelWriter.scala
@@ -1,10 +1,10 @@
 package com.codingkapoor.esslattendancereportgenerator.writer
 
 import java.io.FileOutputStream
-import java.time.YearMonth
+import java.time.{LocalDate, YearMonth}
 
 import com.codingkapoor.esslattendancereportgenerator.`package`._
-import com.codingkapoor.esslattendancereportgenerator.model.{AttendancePerEmployee, Employee, Holiday}
+import com.codingkapoor.esslattendancereportgenerator.model._
 import com.codingkapoor.esslattendancereportgenerator.writer.attendance.AttendanceWriter
 import com.codingkapoor.esslattendancereportgenerator.writer.attendanceheader.AttendanceHeaderWriter
 import com.codingkapoor.esslattendancereportgenerator.writer.companydetails.CompanyDetailsWriter
@@ -13,9 +13,12 @@ import com.codingkapoor.esslattendancereportgenerator.writer.employeeinfoheader.
 import com.codingkapoor.esslattendancereportgenerator.writer.holiday.HolidayWriter
 import com.codingkapoor.esslattendancereportgenerator.writer.sheetheader.SheetHeaderWriter
 import com.codingkapoor.esslattendancereportgenerator.writer.weekend.WeekendWriter
+import com.typesafe.scalalogging.LazyLogging
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
-class ExcelWriter(val month: Int, val year: Int, val attendances: Seq[AttendancePerEmployee], val holidays: Seq[Holiday]) extends SheetHeaderWriter with CompanyDetailsWriter with
+import scala.collection.immutable
+
+class ExcelWriter private(val month: Int, val year: Int, val attendances: Seq[AttendancePerEmployee], val holidays: Seq[Holiday]) extends SheetHeaderWriter with CompanyDetailsWriter with
   EmployeeInfoHeaderWriter with EmployeeInfoWriter with AttendanceHeaderWriter with AttendanceWriter with
   HolidayWriter with WeekendWriter {
   val yearMonth: YearMonth = YearMonth.of(year, month)
@@ -25,7 +28,7 @@ class ExcelWriter(val month: Int, val year: Int, val attendances: Seq[Attendance
 
   val attendanceDimensions: AttendanceDimensions = AttendanceDimensions(month, year, employees)
   val attendanceHeaderDimensions: AttendanceHeaderDimensions = AttendanceHeaderDimensions(month, year)
-  val companyDetailsDimensions: CompanyDetailsDimensions= CompanyDetailsDimensions()
+  val companyDetailsDimensions: CompanyDetailsDimensions = CompanyDetailsDimensions()
   val employeeInfoDimensions: EmployeeInfoDimensions = EmployeeInfoDimensions(employees)
   val employeeInfoHeaderDimensions: EmployeeInfoHeaderDimensions = EmployeeInfoHeaderDimensions()
   val sectionHeaderDimensions: SectionHeaderDimensions = SectionHeaderDimensions(month, year)
@@ -63,8 +66,33 @@ class ExcelWriter(val month: Int, val year: Int, val attendances: Seq[Attendance
   }
 }
 
-object ExcelWriter {
-  def apply(month: Int, year: Int, attendances: Seq[AttendancePerEmployee], holidays: Seq[Holiday]): ExcelWriter = {
+object ExcelWriter extends LazyLogging {
+  def apply(month: Int, year: Int): ExcelWriter = {
+    val holidays = Holiday.getHolidays.filter(h => h.date.getMonthValue == month && h.date.getYear == year)
+    logger.debug(s"holidays = $holidays")
+
+    val employees = Employee.getEmployees
+    logger.debug(s"employees = $employees")
+
+    val attendanceLogs: Map[Int, List[LocalDate]] = AttendanceLog.getAttendanceLogs.foldLeft(Map[Int, List[LocalDate]]()) { (acc, att) =>
+      if (acc.contains(att.empId)) {
+        acc + (att.empId -> (att.date :: acc(att.empId)).sortWith(_.isBefore(_)))
+      } else acc + (att.empId -> List(att.date).sortWith(_.isBefore(_)))
+    }
+    logger.debug(s"attendanceLogs = $attendanceLogs")
+
+    val requests = Request.getRequests
+    logger.debug(s"requests = $requests")
+
+    val attendances: immutable.Seq[AttendancePerEmployee] = employees.map { employee =>
+      val attendance = attendanceLogs(employee.empId) // handle key not found
+    val _requests: List[Request] = requests.filter(l => l.empId == employee.empId)
+      val r = AttendancePerEmployee.getAttendancePerEmployee(employee, attendance, holidays, _requests)(month, year)
+      logger.debug(s"emp = ${employee.empId}, r = ${r.attendance.toList.sortWith(_._1 < _._1)}")
+
+      r
+    }
+
     new ExcelWriter(month, year, attendances, holidays)
   }
 }
